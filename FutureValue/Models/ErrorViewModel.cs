942039247c47b8eaeb6603754f996ee4ec63c136
Capella87 <contact@capelladev.com>
2023-08-14 13:02:43 +0900

    Initial commit

---
+++ FutureValue/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FutureValue.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
