99af6225d6a604dc8fe105e4b649124840b7953c
Capella87 <contact@capelladev.com>
2024-01-19 00:00:02 +0900

    Support HTTP/3 in MinimalWebAPI
    From Windows 11 21H2 (Build 22000), MsQUIC, developed by Microsoft, is built in the operating system, support HTTP/3 in Kestrel server.
    Sadly, Windows 10 does not support it, but still have ability to serve HTTP/2.

--- MinimalWebAPI/Program.cs
+++ MinimalWebAPI/Program.cs
@@ -3,6 +3,17 @@ using Microsoft.AspNetCore.HttpLogging;
 // Create a 'builder' prior to creating WebApplication object for configuration.
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure HTTP/3 for Kestrel (Available on Windows 11 22000.0 or later)
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    // Set port to 2010 now, but need to integrate with appsettings.json later
+    options.ListenAnyIP(2010, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
+
 // Middlewares
 
 // Use development environment logging; Not to be used in production
@@ -14,15 +25,21 @@ builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Debug);
 var app = builder.Build();
 
 app.UseHsts();
+app.UseHttpLogging();
 
 if (app.Environment.IsDevelopment())
 {
+    // In development environment, it includes detailed error information.
     app.UseDeveloperExceptionPage();
-    app.UseHttpLogging();
 }
 else
 {
     // Exception handler for production. Does not leak sensitive information.
+    // When the exception is thrown in endpoint middleware, it is propagated up the pipeline.
+    // The exception handler middleware catches it.
+    // It changes the response path to /error and sends the request down the middleware pipeline again.
+    // The pipeline executes the new error path and a response as usual.
+    // The exception handler middleware updates the new response's status code to 500.
     app.UseExceptionHandler("/error");
 }
 
