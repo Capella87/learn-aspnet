850d49a6093fa4f219878c55aced021e5e1c674a
Capella87 <contact@capelladev.com>
2024-03-04 23:53:44 +0900

    Add DIServicePractice as a project
    Practicing Dependency Injection and services

---
+++ DIServicePractice/Program.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.HttpLogging;
+using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.ComponentModel.DataAnnotations;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Kestrel configuration for HTTP/2 and HTTP/3. This can be replaced to appsettings.json related..
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.ListenAnyIP(2010, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
+
+// Routing configuration
+builder.Services.Configure<RouteOptions>(o =>
+{
+    o.LowercaseUrls = true;
+    o.AppendTrailingSlash = true;
+    o.LowercaseQueryStrings = false;
+});
+
+// JSON configuration
+builder.Services.ConfigureHttpJsonOptions(o =>
+{
+    o.SerializerOptions.AllowTrailingCommas = false;
+    o.SerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    o.SerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
+builder.Services.AddAntiforgery();
+builder.Services.AddHttpLogging(opts =>
+   opts.LoggingFields = HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Debug);
+builder.Services.AddProblemDetails();
+builder.Services.AddRazorPages();
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseHttpLogging();
+app.UseAntiforgery();
+app.UseStatusCodePages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler();
+}
+
+app.MapGet("/", () => "Hello Dependency Injection!");
+
+app.Run();
