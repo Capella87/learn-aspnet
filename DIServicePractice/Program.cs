388147982c872275d98a1ba8b7273fdc93f1d3ae
Capella87 <contact@capelladev.com>
2024-03-07 23:45:47 +0900

    [DIServicePractice] Add more dependencies
    Without Dependency Injection

--- DIServicePractice/Program.cs
+++ DIServicePractice/Program.cs
@@ -65,17 +65,59 @@ app.MapGet("/register/{username}", RegisterUser);
 
 app.Run();
 
+// Multiple dependencies without Dependency Injection
 string RegisterUser(string username)
 {
-    var emailSender = new EmailSender();
+    var emailSender = new EmailSender(
+        new MessageFactory(),
+        new NetworkClient(
+            new EmailServerSettings("asdf.asdf.com", 25)));
     emailSender.SendEmail(username);
     return $"Email sent to {username}!";
 }
 
 public class EmailSender
 {
+    private readonly NetworkClient _client;
+    private readonly MessageFactory _factory;
+
+    public EmailSender(MessageFactory factory, NetworkClient client)
+    {
+        _factory = factory;
+        _client = client;
+    }
+
     public void SendEmail(string username)
     {
+        var email = _factory.Create(username);
+        _client.SendEmail(email);
+
         Console.WriteLine($"Email sent to {username}!");
     }
 }
+
+public class NetworkClient
+{
+    private readonly EmailServerSettings _settings;
+
+    public NetworkClient(EmailServerSettings settings)
+    {
+        _settings = settings;
+    }
+
+    public void SendEmail(Email email)
+    {
+        Console.WriteLine($"Connecting to server {_settings.Host}:{_settings.Port}");
+        Console.WriteLine($"Email sent to {email.Address}: {email.Message}");
+    }
+}
+
+// Factory class; Uses default constructor
+public class MessageFactory
+{
+    public Email Create(string emailAddress)
+        => new Email(emailAddress, "Thanks for signing up!");
+}
+
+public record Email(string Address, string Message);
+public record EmailServerSettings(string Host, int Port);
