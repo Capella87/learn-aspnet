81e8e33b9f9997d07de013c72bdd10a84c0c7f90
Capella87 <capella87@outlook.com>
2024-04-05 23:04:42 +0900

    Add ConfigPractice

---
+++ ConfigPractice/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.HttpLogging;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Kestrel configuration for HTTP/2 and HTTP/3. This can be replaced to appsettings.json related..
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.ListenAnyIP(20621, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2AndHttp3;
+        listenOptions.UseHttps();
+    });
+});
+
+// Routing configuration
+builder.Services.Configure<RouteOptions>(o =>
+{
+    o.LowercaseUrls = true;
+    o.AppendTrailingSlash = true;
+    o.LowercaseQueryStrings = false;
+});
+
+// JSON configuration
+builder.Services.ConfigureHttpJsonOptions(o =>
+{
+    o.SerializerOptions.AllowTrailingCommas = false;
+    o.SerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    o.SerializerOptions.PropertyNameCaseInsensitive = true;
+});
+
+// Enable Scope Validation always (By default, it is only enabled in development)
+builder.Host.UseDefaultServiceProvider(o =>
+{
+    o.ValidateScopes = true;
+    o.ValidateOnBuild = true;
+});
+// It is safe to use only dependencies that have longer than or equal to the service's lifetime.
+
+builder.Services.AddAntiforgery();
+builder.Services.AddHttpLogging(opts =>
+   opts.LoggingFields = HttpLoggingFields.RequestProperties);
+builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Debug);
+builder.Services.AddProblemDetails();
+builder.Services.AddHealthChecks();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.UseHsts();
+app.UseHttpsRedirection();
+app.UseHttpLogging();
+app.UseAntiforgery();
+app.UseStatusCodePages();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler();
+}
+
+app.MapRazorPages();
+
+app.MapGet("/", () => 
+@"Let's learn configuration and generic host!");
+
+app.Run();
