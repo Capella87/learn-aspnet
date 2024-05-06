4b7511c69e03ee9f7a62795d98d2f54567dfd192
Capella87 <capella87@outlook.com>
2024-05-06 18:05:54 +0900

    [EFCorePractice] Add `Ingredient` and `Recipe` classes

---
+++ EFCorePractice/Recipe.cs
@@ -0,0 +1,12 @@
+namespace EFCorePractice
+{
+    public class Recipe
+    {
+        public int RecipeId { get; set; }
+        public required string Name { get; set; }
+        public TimeSpan TimeToCook { get; set; }
+        public bool IsDeleted { get; set; }
+        public required string Method { get; set; }
+        public required ICollection<Ingredient> Ingredients { get; set; }
+    }
+}
