7fe2696351bb9ff639939215f32a6a3050bde455
Capella87 <capella87@outlook.com>
2024-05-08 03:19:18 +0900

    [EFCorePractice] Add Recipe property on Ingredient class
    Source: https://www.learnentityframeworkcore5.com/relationship-in-ef-core

--- EFCorePractice/Ingredient.cs
+++ EFCorePractice/Ingredient.cs
@@ -6,6 +6,9 @@ namespace EFCorePractice
         
         // This represents Many-To-One relationship between Ingredient and Recipe classes
         public int RecipeId { get; set; }
+        // This property is for fully defined relationships
+        // Source: https://www.learnentityframeworkcore5.com/relationship-in-ef-core
+        public Recipe Recipe { get; set; }
         public required string Name { get; set; }
         public decimal Quantity { get; set; }
         public required string Unit { get; set; }
