c3af71c6e09de805abb732d07b05d37d57829820
Capella87 <capella87@outlook.com>
2024-05-09 14:27:38 +0900

    [EFCorePractice] Add more columns regarding vegan on Recipe

--- EFCorePractice/Migrations/20240509051925_ExtraRecipeFields.Designer.cs
+++ EFCorePractice/Migrations/20240509051925_ExtraRecipeFields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFCorePractice.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240509051925_ExtraRecipeFields")]
+    partial class ExtraRecipeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EFCorePractice/Recipe.cs
+++ EFCorePractice/Recipe.cs
@@ -6,6 +6,8 @@ namespace EFCorePractice
         public required string Name { get; set; }
         public TimeSpan TimeToCook { get; set; }
         public bool IsDeleted { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsVegetarian { get; set; }
         public required string Method { get; set; }
         public required ICollection<Ingredient> Ingredients { get; set; }
     }
