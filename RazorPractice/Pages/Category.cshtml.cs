06461fd7e7a21f0011b53098b4b9d011b8f6bb60
Capella87 <capella87@outlook.com>
2024-05-17 15:06:57 +0900

    [RazorPractice] Add ToDo feature

---
+++ RazorPractice/Pages/Category.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ToDoList
+{
+    public class CategoryModel : PageModel
+    {
+        private readonly ToDoService _service;
+        public List<ToDoListModel> Items { get; set; }
+
+        public CategoryModel(ToDoService service)
+        {
+            _service = service;
+        }
+
+        // Controller (handler)
+        public ActionResult OnGet(string category)
+        {
+            Items = _service.GetItemsForCategory(category);
+
+            // Create a new page
+            return Page();
+        }
+    }
+}
