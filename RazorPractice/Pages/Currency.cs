c7e92ee9be79a07fec34e43e8905607cfc90ec0d
Capella87 <capella87@outlook.com>
2024-07-14 17:56:32 +0900

    [RazorPractice] Implement CurrencyModel with LinkGenerator

--- RazorPractice/Pages/Currency.cs
+++ RazorPractice/Pages/Currency.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPractice.Pages
 {
+    // This code is should be located in Controllers, Not here, "Page"
+    // However, the author mentioned for comparison, I put this here in temporary.
     public class Currency : Controller
     {
         [HttpGet("currency/index")]
@@ -16,6 +20,33 @@ namespace RazorPractice.Pages
         public IActionResult View(string code)
         {
             return View();
+        }
+    }
+
+    // Fit for here
+    public class CurrencyModel : PageModel
+    {
+        private readonly LinkGenerator _link;
+        // Access using Dependency Injection
+        public CurrencyModel(LinkGenerator linkGenerator)
+        {
+            _link = linkGenerator;
+        }
+
+        public void OnGet()
+        {
+            var url1 = Url.Page("Currency/View", new { id = 5 });
+            // Equivalent to Url.Page; Generates a relative URL
+            var url3 = _link.GetPathByPage(HttpContext, "/Currency/View", values: new { id = 5 });
+            var url2 = _link.GetPathByPage("/Currency/View", values: new { id = 5 });
+
+            // Absolute URL
+            var url4 = _link.GetUriByPage(
+                page: "/Currency/View",
+                handler: null,
+                values: new { id = 5 },
+                scheme: "https",
+                host: new HostString("example.com"));
         }
     }
 }
