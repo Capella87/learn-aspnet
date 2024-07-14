7f6d389fc6764ac877056261a627df3eeffc01e4
Capella87 <capella87@outlook.com>
2024-07-14 17:15:35 +0900

    [RazorPractice] Implement Currency routing with `IUrlHelper`
    In this code, Url.Action is an implementation of `IUrlHelper`

--- RazorPractice/Pages/Currency.cs
+++ RazorPractice/Pages/Currency.cs
@@ -7,6 +7,14 @@ namespace RazorPractice.Pages
         [HttpGet("currency/index")]
         public IActionResult Index()
         {
+            var url = Url.Action("View", "Currency",
+                new { code = "USD" });
+            return Content($"The URL is {url}");
+        }
+
+        [HttpGet("currency/view/{code}")]
+        public IActionResult View(string code)
+        {
             return View();
         }
     }
