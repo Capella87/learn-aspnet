fe714b530483c561dd488f66cdfca085776871aa
Capella87 <capella87@outlook.com>
2025-02-05 17:38:35 +0900

    [ControllerWebAPI] Remove unnecessary comment in GameService.cs

--- ControllerWebAPI/GameService.cs
+++ ControllerWebAPI/GameService.cs
@@ -87,22 +87,4 @@ public class GameService
 
         return new GameViewModel(game);
     }
-
-    // public async AddNewGame()
-
-    //private readonly List<Game> _games =
-    //[
-    //    new Game { Id = "aoe2", Name = "Age of Empires 2: Definitive Edition" },
-    //    new Game { Id = "aoe3", Name = "Age of Empires 3: Definitive Edition" },
-    //    new Game { Id = "aom", Name = "Age of Mythology: Retold" },
-    //    new Game { Id = "sdv", Name = "Stardew Valley" },
-    //    new Game { Id = "ts3", Name = "The Sims 3" },
-    //    new Game { Id = "ts2", Name = "The Sims 2" },
-    //    new Game { Id = "ets", Name = "Euro Truck Simulator 2" },
-    //    new Game { Id = "f4", Name = "Fallout 4" },
-    //    new Game { Id = "f3", Name = "Fallout 3" },
-    //    new Game { Id = "fnv", Name = "Fallout: New Vegas" },
-    //    new Game { Id = "tropico4", Name = "Tropico 4" },
-    //    new Game { Id = "halo", Name = "Halo: Master Chief Collection" },
-    //];
 }
