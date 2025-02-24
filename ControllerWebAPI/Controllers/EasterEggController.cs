de60e0daa47a46272804c6aac1849bcfe46749a2
Capella87 <capella87@outlook.com>
2025-02-24 18:19:05 +0900

    [ControllerWebAPI] Add response type attributes on EasterEggController

--- ControllerWebAPI/Controllers/EasterEggController.cs
+++ ControllerWebAPI/Controllers/EasterEggController.cs
@@ -5,6 +5,7 @@ namespace ControllerWebAPI.Controllers;
 public class EasterEggController : ControllerBase
 {
     [HttpGet("/teapot")]
+    [ProducesResponseType(StatusCodes.Status418ImATeapot)]
     public async Task<IActionResult> Index()
     {
         return await Task.FromResult(StatusCode(StatusCodes.Status418ImATeapot, "I'm a teapot"));
