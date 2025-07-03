40795029d5778a70160932d5c3efc65f00cba5ea
Capella87 <capella87@outlook.com>
2025-07-03 11:54:27 +0900

    Add IdentityFromScratch project
    Note: This project is for API server

---
+++ IdentityFromScratch/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityFromScratch.Controllers;
+
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    public IActionResult Index()
+    {
+        return new OkObjectResult(new { Message = "Welcome!" });
+    }
+}
