3d0ae08c57190e8b75859d139c5016791faa91a3
Capella87 <capella87@outlook.com>
2025-07-13 14:42:27 +0900

    Rename UserController.cs and implement Login action

---
+++ IdentityFromScratch/Controllers/AuthController.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using IdentityFromScratch.Identity;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace IdentityFromScratch.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(SignInManager<IdentityUser> signInManager, ILogger<AuthController> logger)
+    {
+        _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    // Login
+    [Route("/login")]
+    [ProducesResponseType(typeof(IdentityResult), StatusCodes.Status200OK)]
+    [HttpPost]
+    public async Task<IActionResult> Login([FromBody] LoginDto data)
+    {
+        _signInManager.AuthenticationScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        // Convert Base64 password to plain password.
+
+        var result = await _signInManager.PasswordSignInAsync(data.Username, data.Password, false, false);
+
+        if (!result.Succeeded)
+        {
+            return Problem(result.ToString(), statusCode: StatusCodes.Status401Unauthorized);
+        }
+
+        // All authentication and authorization works are done in SignInManager.
+        return Ok(result);
+    }
+
+    // Logout
+
+    // Sign Up
+
+    // Confirm Email
+
+    // Update user information
+
+    // Password Reset
+
+    // Delete User Account
+}
